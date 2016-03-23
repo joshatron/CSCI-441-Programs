51b0d09382597bc2221eb094bbc00c79ceb2bbea
Joshua Leger <legermjoshua@gmail.com>
2016-03-23 16:00:23 -0600

    rectangle working

--- program2/shape.cpp
+++ program2/shape.cpp
@@ -57,42 +57,42 @@ void Shape::updateBrickLocsRect(double brickWidth, double brickHeight, double br
 {
     int width = (int)(scaleX * xDiameter);
     int depth = (int)(scaleZ * zDiameter);
-    double startX = (-1 * (width / 2.) * (brickWidth + spacing)) + (spacing / 2) + (brickWidth / 2);
-    double startZ = (-1 * (depth / 2.) * (brickWidth + spacing)) + (spacing / 2) + (brickWidth / 2);
+    double startX = -1 * (((width - 1) / 2.) * (brickWidth + spacing));
+    double startZ = -1 * (((depth - 1) / 2.) * (brickWidth + spacing));
 
     bool even = starting;
     float yLoc = scaleY * startY;
     for(int k = 0; k < (int)(scaleY * height); k++)
     {
-        float loc = startX + brickDepth;
+        float loc = startX;
         int widthNum = width;
         if(even)
         {
-            loc += brickDepth + spacing;
+            loc += (brickWidth + spacing) * .5;
             widthNum--;
         }
 
         for(int a = 0; a < widthNum; a++)
         {
-            brickLocs.push_back(translate(mat4(1.f), vec3(loc, yLoc, startZ)) * rotate(mat4(1.f), (float)angle, vec3(0,1,0)));
-            brickLocs.push_back(translate(mat4(1.f), vec3(loc, yLoc, startZ * -1)) * rotate(mat4(1.f), (float)angle, vec3(0,1,0)));
-            loc += brickWidth + spacing / 2;
+            brickLocs.push_back(translate(mat4(1.f), vec3(loc, yLoc, startZ - (brickDepth / 2))) * rotate(mat4(1.f), (float)angle, vec3(0,1,0)));
+            brickLocs.push_back(translate(mat4(1.f), vec3(loc, yLoc, startZ * -1 + (brickDepth / 2))) * rotate(mat4(1.f), (float)angle, vec3(0,1,0)));
+            loc += brickWidth + spacing;
         }
 
-        loc = startZ + brickDepth;
+        loc = startZ;
         int depthNum = depth;
         if(!even)
         {
-            loc += brickDepth + spacing;
+            loc += (brickWidth + spacing) * .5;
             depthNum--;
         }
         even = !even;
 
         for(int a = 0; a < depthNum; a++)
         {
-            brickLocs.push_back(translate(mat4(1.f), vec3(startX, yLoc, loc)) * rotate(mat4(1.f), (float)(90. + angle), vec3(0,1,0)));
-            brickLocs.push_back(translate(mat4(1.f), vec3(startX * -1, yLoc, loc)) * rotate(mat4(1.f), (float)(90. + angle), vec3(0,1,0)));
-            loc += brickWidth + spacing / 2;
+            brickLocs.push_back(translate(mat4(1.f), vec3(startX - (brickDepth / 2), yLoc, loc)) * rotate(mat4(1.f), (float)(90. + angle), vec3(0,1,0)));
+            brickLocs.push_back(translate(mat4(1.f), vec3(startX * -1 + (brickDepth / 2), yLoc, loc)) * rotate(mat4(1.f), (float)(90. + angle), vec3(0,1,0)));
+            loc += brickWidth + spacing;
         }
 
         yLoc += brickHeight + spacing;
