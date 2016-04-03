3e7b34830f3cde4602094e2dee5a1e4c0f7d68ad
Joshua Leger <legermjoshua@gmail.com>
2016-04-02 20:42:19 -0600

    structure tranform working now too

--- program2/structure.cpp
+++ program2/structure.cpp
@@ -7,9 +7,9 @@ void Structure::updateBrickLocs(double brickWidth, double brickHeight, double br
     for(unsigned int k = 0; k < shapes.size(); k++)
     {
         shapes.at(k).updateBrickLocs(brickWidth, brickHeight, brickDepth, spacing, scaleX, scaleY, scaleZ, wallDepth);
-        mat4 toMove = translate(mat4(1.f), vec3(shapes.at(k).centerLoc.x * scaleX + (shapes.at(k).centerLoc.x * scaleX * spacing),
-                                                shapes.at(k).centerLoc.y * scaleY + (shapes.at(k).centerLoc.y * scaleY * spacing),
-                                                shapes.at(k).centerLoc.z * scaleZ + (shapes.at(k).centerLoc.z * scaleZ * spacing))) *
+        mat4 toMove = translate(mat4(1.f), vec3((shapes.at(k).centerLoc.x * scaleX * (spacing + brickWidth)),
+                                                (shapes.at(k).centerLoc.y * scaleY * (spacing + brickWidth)),
+                                                (shapes.at(k).centerLoc.z * scaleZ * (spacing + brickWidth)))) *
                       rotate(mat4(1.f), (float)shapes.at(k).angle, vec3(0, 1, 0));
         for(unsigned int a = 0; a < shapes.at(k).brickLocs.size(); a++)
         {
